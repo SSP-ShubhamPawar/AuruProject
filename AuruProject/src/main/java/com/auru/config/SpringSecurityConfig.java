84aa1f679a29fc06c4987d8a329ee07e419f6a91
Shubham Pawar <37012907+SSP-ShubhamPawar@users.noreply.github.com>
2024-10-01 10:52:14 +0530

    Add files via upload
    Cmmited changes

--- AuruProject/src/main/java/com/auru/config/SpringSecurityConfig.java
+++ AuruProject/src/main/java/com/auru/config/SpringSecurityConfig.java
@@ -1,44 +1,47 @@
 package com.auru.config;
 
-//@EnableWebSecurity
-public class SpringSecurityConfig{/*
-extends WebSecurityConfigurerAdapter {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-	@Autowired
-	DataSource datasource;
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	//private PasswordEncoder NoOpPasswordEncoder;
+	// @Autowired
+	// DataSource datasource;
 
 	// For authentication Authentication manger need to override
-	  
-	  @Override protected void configure(AuthenticationManagerBuilder auth) throws
-	  Exception { PasswordEncoder encoder =
-	  PasswordEncoderFactories.createDelegatingPasswordEncoder();
-	  auth.inMemoryAuthentication().withUser("shubham").password(encoder.encode(
-	  "shubham")).roles("TEAM LEAD") .and()
-	  .withUser("snehal").password(encoder.encode("snehal")).roles("HR");
-	  
-	  }
-	  
-	  
-	  
-	  @Bean 
-	  PasswordEncoder getPasswordEncoder() 
-	  { return NoOpPasswordEncoder
-	  authenticationManagerBean(); 
-	  }
-	  
-	  
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication()
+				.withUser("shubham").password(encoder.encode("shubham")).roles("TEAM LEAD").and()
+				.withUser("snehal").password(encoder.encode("snehal")).roles("HR");
+
+	}
+
+	/*
+	 * @Bean PasswordEncoder getPasswordEncoder() { return NoOpPasswordEncoder
+	 * authenticationManagerBean(); }
+	 */
+
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-				// .anyRequest().permitAll().and().formLogin(); This method gives access t all
-				// the POST request
-				.antMatchers("/api/employees").hasRole("ADMIN")
-				.antMatchers("/").permitAll().and()
-				.formLogin();
+				 .anyRequest().permitAll().and().formLogin(); //This method gives access t all the POST request
+				//.antMatchers("/addEvent").hasRole("TEAM LEAD").antMatchers("/").permitAll().and().formLogin();
 	}
-	 @Override public void configure(WebSecurity web) throws Exception {
-		 
-		  web.ignoring().antMatchers("/h2-console/**"); 
-		  }
 
-*/
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+		//web.ignoring().antMatchers("/h2-console/**");
 	}
+
+}
